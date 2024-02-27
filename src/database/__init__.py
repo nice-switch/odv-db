f3dacfabc4f8111c93138388948b0fd040b50ad8
nice-switch <kevin@senpai.zone>
2024-02-26 20:18:01 -0500

    typin' away

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,8 +1,32 @@
 from database import model, interface, secure
 
 
+# TODO include email method to retrieve an account.
 def get_account(username: str) -> interface.AccountInterface | None:
-    pass
+    """Retrieve an account using the username.
+
+    Args:
+        username (str): Target username to find the account by.
+
+    Returns:
+        interface.AccountInterface | None: Returns AccountInterface if successful.
+    """
+    account_model: model.Account | None = None
+
+    try:
+        # TODO include use for model.change_database_to later after I make my planned changes.
+        account_model = model.Account.get(
+            username==username
+        )
+    except Exception as _:
+        pass
+
+    if account_model:
+        return interface.AccountInterface(
+            account_model=account_model
+        )
+    
+    return None
 
 
 def create_account(username: str, password: str) -> interface.AccountInterface | None:
--- src/database/interface.py
+++ src/database/interface.py
@@ -1,5 +1,7 @@
 from database import model
 
+# NOTE When making changes to the data of models, ensure model.change_database_to is used.
+# God forbid I mix production and development data.
 
 class AccountInterface():
     def __init__(self, account_model: model.Account):
